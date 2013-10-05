1b660d6a31e57020e15ef1fc6a7f9ba9ac6b1a7f
qpaulson <qpaulson@gmail.com>
2013-10-04 22:09:18 -0700

    removed the old weather stuff from voiceExec

--- voiceExec.py
+++ voiceExec.py
@@ -169,70 +169,6 @@ def cleanup():
     print "... Deleting any tmp audio files lying around"
     os.system( "rm output_*" )
 
-#def speakWeather( string ):
-        #location_code = vConfig.get( "weather", "weather_location_code" )
-#
-	#weather_com_result = pywapi.get_weather_from_weather_com( str(location_code ))
-	#print weather_com_result
-	#print string
-#
-#
-	#lookupString = ''
-	#### TODAY
-	#if ( re.compile( "today", re.IGNORECASE ).findall( string ,1 )):
-	    #todayData = weather_com_result['forecasts'][0]
-	    #if ( todayData['day']['text'] != 'N/A' ):
-		    #if ( int( todayData['day']['chance_precip'] ) > 40 ):
-		        #lookupString = "Today will be " + str( todayData['day']['text'] ) + " with a chance of showers and a high of " + str( todayData['high'] ) + "degrees"
-		    #else:
-		        #lookupString = "Today will be " + str( todayData['day']['text'] ) + " with a high of " + str( todayData['high'] ) + "degrees"
-            #else:
-		    #if ( int(todayData['night']['chance_precip'] ) > 40 ):
-		        #lookupString = "Tonight will be " + str( todayData['night']['text'] ) + " with a chance of showers"
-		    #else:
-		        #lookupString = "Tonight will be " + str( todayData['night']['text'] )
-#
-#
-	### TONIGHT
-	#elif ( re.compile( "tonight", re.IGNORECASE).findall( string ,1 )):
-	    #todayData = weather_com_result['forecasts'][0]
-	    #if ( int(todayData['night']['chance_precip'] ) > 40 ):
-	        #lookupString = "Tonight will be " + str( todayData['night']['text'] ) + " with a chance of showers"
-	    #else:
-	        #lookupString = "Tonight will be " + str( todayData['night']['text'] )
-#
-	#### Tomorrow Night
-	#elif ( re.compile( "tomorrow night", re.IGNORECASE).findall( string ,1 )):
-	    #todayData = weather_com_result['forecasts'][1]
-	    #if ( int(todayData['night']['chance_precip'] ) > 40 ):
-	        #lookupString = "Tomorrow night will be " + str( todayData['night']['text'] ) + " with a chance of showers"
-	    #else:
-	        #lookupString = "Tomorrow night will be " + str( todayData['night']['text'] )
-
-	#### TODAY
-	#elif ( re.compile( "tomorrow", re.IGNORECASE ).findall( string ,1 )):
-	    #todayData = weather_com_result['forecasts'][1]
-	    #if ( todayData['day']['text'] != 'N/A' ):
-		    #if (( int( todayData['day']['chance_precip'] ) > 40 ) or ( int( todayData['night']['chance_precip'] ) > 40 )):
-		        #lookupString = "Tomorrow will be " + str( todayData['day']['text'] ) + " with a chance of showers and a high of " + str( todayData['high'] ) + " degrees"
-		    #else:
-		        #lookupString = "Tomorrow will be " + str( todayData['day']['text'] ) + " with a high of " + str( todayData['high'] ) + "degrees"
-#
-#
-	#else:
-	    #lookupString = "It is currently " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "degrees.\n\n" 
-#
-#
-	#print lookupString
-	### Work our magic
-	#if ( lookupString != '' ):
-	    #GoogleSpeech.tts( lookupString )
-	#else: 
-	    #GoogleSpeech.tts( "Sorry, Weather information un-available at this time, please try again later" )
-        
-
-
-	
 
 if(__name__ == '__main__'):
     atexit.register(cleanup)
@@ -242,5 +178,3 @@ if(__name__ == '__main__'):
     vConfig = VoiceConfig()
     listen_for_speech()
     
-
-
